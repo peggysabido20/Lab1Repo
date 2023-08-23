3712c85b83cdb793e74ed70dc9a3f4ebbe2b7b02
Peggy Sabido <peggy_sabido@hotmail.com>
2023-08-22 20:06:03 -0400

    Lab1 Commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("How many sodas have been sold today? 100 in stock: ");
+        int iSodas = int.Parse(Console.ReadLine());
+        if (iSodas > 100)
+        {
+            Console.WriteLine("That value is too high. Stock not adjusted.");
+        }
+        else
+        {
+            int iSodasAdj = 100 - iSodas;
+            Console.WriteLine("There are " + iSodasAdj + " Sodas left.");
+        }
+
+        Console.Write("How many Chips have been sold today? 40 are in stock: ");
+        int iChips = int.Parse(Console.ReadLine());
+        if (iChips > 40)
+        {
+            Console.WriteLine("That value is too high. Stock not adjusted.");
+        }
+        else
+        {
+            int iChipsAdj = 40 - iChips;
+            Console.WriteLine("There are " + iChipsAdj + " Chips left.");
+        }
+
+        Console.Write("How many Candy have been sold today? 60 are in stock: ");
+        int iCandy = int.Parse(Console.ReadLine());
+        if (iCandy > 60)
+        {
+            Console.WriteLine("That value is too high. Stock not adjusted.");
+        }
+        else
+        {
+            int iCandyAdj = 60 - iCandy;
+            Console.WriteLine("There are " + iCandyAdj + " Candies left.");
+        }
+    }
+}
+
