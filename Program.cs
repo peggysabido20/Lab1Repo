0b4988d8e0546231bb3bd57611284766b21d1362
Peggy Sabido <peggy_sabido@hotmail.com>
2023-08-24 14:18:01 -0400

    Second submit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 public class Program
 {
     public static void Main()
     {
+        int iSodasAdj = 0;
+        int iChipsAdj = 0;
+        int iCandyAdj = 0;
+        bool bSodasRes = false;
+        bool bChipsRes = false;
+        bool bCandyRes = false;
         Console.Write("How many sodas have been sold today? 100 in stock: ");
         int iSodas = int.Parse(Console.ReadLine());
         if (iSodas > 100)
@@ -12,8 +19,12 @@ public class Program
         }
         else
         {
-            int iSodasAdj = 100 - iSodas;
+            iSodasAdj = 100 - iSodas;
             Console.WriteLine("There are " + iSodasAdj + " Sodas left.");
+            if (iSodasAdj <= 40)
+            {
+                bSodasRes = true;
+            }
         }
 
         Console.Write("How many Chips have been sold today? 40 are in stock: ");
@@ -24,8 +35,12 @@ public class Program
         }
         else
         {
-            int iChipsAdj = 40 - iChips;
+            iChipsAdj = 40 - iChips;
             Console.WriteLine("There are " + iChipsAdj + " Chips left.");
+            if (iChipsAdj <= 20)
+            {
+                bChipsRes = true;
+            }
         }
 
         Console.Write("How many Candy have been sold today? 60 are in stock: ");
@@ -36,8 +51,30 @@ public class Program
         }
         else
         {
-            int iCandyAdj = 60 - iCandy;
+            iCandyAdj = 60 - iCandy;
             Console.WriteLine("There are " + iCandyAdj + " Candies left.");
+            if (iCandyAdj <= 40)
+            {
+                bCandyRes = true;
+            }
+        }
+        
+        if (bSodasRes || bChipsRes || bCandyRes)
+        {
+            Console.WriteLine("Thank you for filling out the values.Here’s what needs to be restocked:");            if (bSodasRes)
+            {
+                Console.WriteLine("Sodas need to be restocked");
+            }            if (bChipsRes)
+            {
+                Console.WriteLine("Chips need to be restocked");
+            }            if (bCandyRes)
+            {
+                Console.WriteLine("Candies need to be restocked");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Nothing needs to be restocked.");
         }
     }
 }
